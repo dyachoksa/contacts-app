9e878d3fac9f47818cab80ae3eac987230def218
Serhii Diachok <sergey@sdyachok.com.ua>
2021-02-14 12:21:30 +0200

    add 404 and contact details pages

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, abort
+
+from app.services.contact_service import ContactService
+
+DATABASE_URL = os.environ.get("DATABASE_URL", "data/contacts.json")
+
 
 app = Flask(__name__)
+
+contact_service = ContactService(DATABASE_URL)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    contacts = contact_service.get_contacts()
+    return render_template("index.html", contacts=contacts)
 
 
 @app.route("/about-us")
@@ -15,4 +30,9 @@ def about():
 
 @app.route("/contacts/<name>")
 def contact_details(name):
-    return f"Contact details for {name}"
+    try:
+        contact = contact_service.find(name)[0]
+    except IndexError:
+        return abort(404)
+
+    return render_template("contact_details.html", contact=contact)
